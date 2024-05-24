cb4ca3257fec9eff0fd3d6d4296b745a9cc50f62
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-05-24 16:12:43 +0530

    Learned Method reference, filter

--- FunctionalProgramming/src/Main.java
+++ FunctionalProgramming/src/Main.java
@@ -3,13 +3,29 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		functionalStyle(List.of(12,13,24,45,8,6,3,6,4,7,8));		
-	}
-	public static void print(int number) {
-		System.out.println(number);
+		List<Integer> numbersList = List.of(12,13,24,45,8,6,3,6,4,7,8);
+		
+		System.out.println("Printing All Numbers");
+		functionalStylePrintNumber(numbersList);
+		
+		System.out.println("Printing Even Numbers");
+		functionalStylePrintEvenNumber(numbersList);
 	}
 
-	private static void functionalStyle(List<Integer> of) {
-		of.stream().forEach(Main::print);
+	/*public static void print(int number) { System.out.println(number); }*/
+	
+	public static boolean isEven(int number) {
+		return number%2==0; // check if number is even or not
+	}
+	
+	private static void functionalStylePrintNumber(List<Integer> numbers) {
+		numbers.stream() // one number at a time
+		.forEach(System.out::println); // Method Reference - to call static we need to mention class name
+	}
+	
+	private static void functionalStylePrintEvenNumber(List<Integer> numbers) {
+		numbers.stream()
+		.filter(Main::isEven) // filter & method reference
+		.forEach(System.out::println); // 'println' is a static method in 'System.out'
 	}
 }
