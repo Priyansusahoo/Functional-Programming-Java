71a82bb961fa2afe877449d3009be67de52df742
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-05-25 00:04:50 +0530

    optimized using Lambda & inbuild methods

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -3,12 +3,13 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		List<Integer> numbers = List.of(1,2);
-		System.out.println(sumOfNumbers(numbers));
+		List<Integer> numbers = List.of(1,2,3);
+		System.out.println("The sum is = " + sumOfNumbers(numbers));
 	}
 	
 	public static int sumOfNumbers(List<Integer> numbers) {
 		return numbers.stream()
-		.reduce(0, (a,b) -> a+b); // reduce - return sum of numbers
+				.reduce(0, Integer::sum);
+//		.reduce(0, (a,b) -> a+b); // reduce - lambda - return sum of numbers
 	}
 }
